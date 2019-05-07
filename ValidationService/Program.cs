b2f349574a421b2e63a0ab45bc43ca71cd3caa15
kaxopas <andre.cachopas@gmail.com>
2019-05-07 23:32:22 +0100

    aula

---
+++ ValidationService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValidationService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
